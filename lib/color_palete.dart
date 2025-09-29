b2f2ca55000d5d24573f1a9c154666952a8f6001
Sandinu Weerathunga <sandinuweerathunga3@gmail.com>
2025-09-29 17:53:57 +0530

    Add macOS and iOS support, implement color palette, and create home screen

---
+++ lib/color_palete.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette {
+  static const Color mainFontColor = Color.fromRGBO(19, 61, 95, 1);
+  static const Color firstSuggestionBoxColor =  Color.fromRGBO(165, 231, 244, 1);
+  static const Color secondSuggestionBoxColor = Color.fromRGBO(157, 202, 235, 1);
+  static const Color thirdSuggestionBoxColor = Color.fromRGBO(162, 238, 239, 1);
+  static const Color assistentCircleColor = Color.fromRGBO(162, 238, 239, 1);
+  static const Color borderColor = Color.fromRGBO(200, 200, 200, 1);
+  static const Color blackColor = Colors.black;
+  static const Color whiteColor = Colors.white;
+}
